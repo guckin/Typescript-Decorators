943b210c57b1526efe18d9e72a95856aae650779
Stephen McGuckin <stephen.mcguckin@dat.com>
2019-07-17 10:43:59 -0700

    Changed everything to mocha

--- test/decorators.test.ts
+++ test/decorators.test.ts
@@ -1,11 +1,19 @@
 import {logMethod} from '../src/decorators';
 
-class TestClass {
-    @logMethod
-    async foo(...args: string[]): Promise<string> {
-        console.log('Heres are args you passed me:', args);
-        return 'bar'
-    }
-}
-const myInstance = new TestClass();
-myInstance.foo('baz');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        // TODO: fix this so it actually asserts something
+        it('It logs stuff', function() {
+            class TestClass {
+                @logMethod
+                async foo(...args: string[]): Promise<string> {
+                    console.log('Heres are args you passed me:', args);
+                    return 'bar'
+                }
+            }
+            const myInstance = new TestClass();
+            myInstance.foo('baz');
+        });
+    });
+});
+
